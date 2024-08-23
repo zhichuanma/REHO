baded1b76e1cb317845c3ef1d9a5cf0f97078ca0
Zhichuan Ma <mzc12311@gmail.com>
2024-08-23 18:26:37 +0200

    Initialzation

--- scripts/examples/3a_Read_csv.py
+++ scripts/examples/3a_Read_csv.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     # Set scenario
     scenario = dict()
-    scenario['Objective'] = 'TOTEX'
+    scenario['Objective'] = 'CCEQL'
     scenario['name'] = 'totex'
     scenario['exclude_units'] = ['Battery', 'NG_Cogeneration']
     scenario['enforce_units'] = []
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     units = infrastructure.initialize_units(scenario, grids)
 
     # Set method options
-    method = {'building-scale': True}
+    method = {'building-scale': True, 'save_lca': True}
 
     # Run optimization
     reho = REHO(qbuildings_data=qbuildings_data, units=units, grids=grids, cluster=cluster, scenario=scenario, method=method, solver="gurobi")
